d1a4e326105c9028f72b1f06d4aef93d4b2e52ce
Wenceslas BARIDON <wbaridon@e3r1p9.42.fr>
2019-06-02 14:26:13 +0200

    Add Basic CSS to todoList

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
 <template>
-  <section class="container">
-      {{list}}
+  <section>
+    <v-layout v-for="item in list" :key="item.id">
+     <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-card-title primary-title>
+            <h3 class="headline mb-0">{{item.title}}</h3>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn flat color="blue">En lire +</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn flat color="orange">Modifier</v-btn>
+          <v-btn flat color="red">Delete</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
   </section>
 </template>
 
